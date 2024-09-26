80cdcf9ee0232939d2926cb006bbf1d311ccce1b
valeryboyko <valerybojko@gmail.com>
2024-09-26 15:47:23 +0300

    KS-1-2 version1

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -1,4 +1,11 @@
 package lesson_1
 fun main(){
-    println("Тестовое решение к уроку 1 задаче 2")
+//    println("Тестовое решение к уроку 1 задаче 2")
+    val numberOfPhone :Int = 75
+val notificationText : String = "Благодарим за покупку,приходите к нам еще"
+
+    println(numberOfPhone)
+    println(notificationText)
+
+
 }
